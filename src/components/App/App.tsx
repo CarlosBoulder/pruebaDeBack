d70811abfca9cdd3bd3f01790c324541cee5f2da
Carlos ALvarez <carlosmail1978@gmail.com>
2023-05-14 16:03:03 +0200

    Add dispatch to get data from api

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,4 +1,24 @@
+import { useEffect } from "react";
+import useApi from "../../hooks/useApi";
+import { useAppDispatch } from "../../store";
+import { loadRobotsActionCreator } from "../../store/robots/robotsSlice";
+
 const App = (): JSX.Element => {
+  const { getRobots } = useApi();
+
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const robots = await getRobots();
+        dispatch(loadRobotsActionCreator(robots));
+      } catch {
+        return;
+      }
+    })();
+  }, [dispatch, getRobots]);
+
   return <h1>Bird Robots</h1>;
 };
 
