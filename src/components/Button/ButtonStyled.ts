0d82f11616c7021fd5f20b93fcae57f4578e7d02
Luis <hola@luiscaballe.com>
2023-05-16 19:19:57 +0200

    Add ButtonStyled

---
+++ src/components/Button/ButtonStyled.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+const ButtonStyled = styled.button`
+  background-color: #e52575;
+  padding: 10px 20px;
+  border-radius: 5px;
+  color: white;
+`;
+
+export default ButtonStyled;
